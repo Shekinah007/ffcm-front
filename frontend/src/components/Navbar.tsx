b92fb2b8a838cb759582eb8897e4457f1b5e80d1
Shekinah Shalom-Hope <shalomsheks@gmail.com>
2023-08-21 05:44:14 +0100

    Add fontawesome link for icons

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { useState } from 'react';
 
+import shieldLogo from "../assets/images/shieldLogo.png"
+
 interface Menu {
     open: boolean,
 }
@@ -11,17 +13,28 @@ const Navbar = () => {
     const [open, setOpen] = useState<boolean>(true)
 
     return (
-        <div className='flex flex-row gap-2 justify-between w-screen items-center z-20  p-3  md:px-10 bg-slate-900 text-gray-400'>
-            <p className="">Flaming Faith Covenant Ministries</p>
-            <div className="menu-btn md:hidden" onClick={(): void => { setOpen(prev => !prev); console.log(open) }} >Menu</div>
-            <div className={`nav -translate-y-72  duration-500 ${open && '-translate-y-0'} p-3 flex flex-col fixed top-10 left-0 bg-slate-900 gap-3 w-screen md:w-auto  md:static  md:flex-row `} >
-                <a href="#">Home</a>
-                <a href="#">About</a>
-                <a href="#">Sermons</a>
-                <a href="#">Gallery</a>
-                <a href="#">Contact</a>
+        <div className='flex fixed w-screen flex-row gap-2 justify-between  items-center z-20  p-3  md:px-10 bg-black text-white'>
+            <div className="" >
+                <img src={shieldLogo} alt="logo home" className='h-16 hidden md:block' />
             </div>
-        </div>
+            <div>
+                {/* <span className="fa-stack fa-lg"><i className="fa fa-circle fa-stack-2x "></i><i className="fa text-black fa-facebook fa-stack-1x fa-inverse"></i></span>
+                <span className="fa-stack fa-lg"><i className="fa fa-circle fa-stack-2x"></i><i className="fa fa-twitter text-black fa-stack-1x fa-inverse"></i></span>
+                <span className="fa-stack fa-lg"><i className="fa fa-circle fa-stack-2x"></i><i className="fa fa-instagram  text-black fa-stack-1x fa-inverse"></i></span> */}
+                <span className="menu-btn md:hidden" onClick={(): void => { setOpen(prev => !prev); console.log(open) }}>
+                    Menu
+                    <i className="fa fa-2x fa-bars"></i>
+                </span>
+
+            </div>
+            <div className={`nav  h-0 overflow-hidden py-0 duration-300  ${open && '-translate-y-0 h-48'} md:p-3  md:py-3  md:h-10 flex flex-col fixed top-10 left-0 bg-black gap-3 w-screen md:w-auto  md:static  md:flex-row `} >
+                <a href="#"> - Home</a>
+                <a href="#"> - About</a>
+                <a href="#"> - Sermons</a>
+                <a href="#"> - Gallery</a>
+                <a href="#"> - Contact</a>
+            </div>
+        </div >
     )
 }
 
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -4,12 +4,19 @@ import logo1 from "../assets/images/ffcmLogo.png"
 const Home = () => {
     return (
         <div>
+            <div className="overlay h-screen w-screen fixed top-0 bg-black opacity-40 "></div>
             <div>
-                <div className="bg-bg1 bg-cover h-screen flex justify-center items-center">
-                    <img alt="logo" className="h-36" src={logo1} />
+                <div className="bg-bg1 flex flex-col gap-4 bg-cover h-screen text-lg md:text-xl text-gray-300 justify-center items-center">
+                    <img alt="logo" className="h-36 z-10" src={logo1} />
+                    <div className="flex flex-col z-10 items-center">
+                        <p className="italic text-gray-300 text-base">- We Walk By Faith And Not By Sight -</p>
+                    </div>
                 </div>
+                <p>Hello</p>
+                <p>Hello</p>
+                <p>Hello</p>
+                <p>Hello</p>
             </div>
-            <button onClick={(): void => { alert("Hello wordl") }}>Say Hello!</button>
         </div>
     )
 }
