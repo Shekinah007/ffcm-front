600e3ab13c2ccd80fb40ebd599093d29f3804b60
Shekinah Shalom-Hope <shalomsheks@gmail.com>
2023-08-21 06:33:25 +0100

    Change nav menu style

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -13,48 +13,27 @@ const Navbar = () => {
     const [open, setOpen] = useState<boolean>(true)
 
     return (
-        <div className='flex fixed w-screen flex-row gap-2 justify-between  items-center z-20  p-3  md:px-10 bg-black text-white'>
+        <div className='justify-center flex fixed w-screen flex-col md:flex-row  md:justify-between min-h-[40px] items-center z-20 px-3 md:p-2  md:px-10 bg-white/20 text-white border-b-2 border-white'>
             <div className="" >
                 <img src={shieldLogo} alt="logo home" className='h-16 hidden md:block' />
             </div>
-            <div>
-                {/* <span className="fa-stack fa-lg"><i className="fa fa-circle fa-stack-2x "></i><i className="fa text-black fa-facebook fa-stack-1x fa-inverse"></i></span>
-                <span className="fa-stack fa-lg"><i className="fa fa-circle fa-stack-2x"></i><i className="fa fa-twitter text-black fa-stack-1x fa-inverse"></i></span>
-                <span className="fa-stack fa-lg"><i className="fa fa-circle fa-stack-2x"></i><i className="fa fa-instagram  text-black fa-stack-1x fa-inverse"></i></span> */}
-                <span className="menu-btn md:hidden" onClick={(): void => { setOpen(prev => !prev); console.log(open) }}>
-                    Menu
-                    <i className="fa fa-2x fa-bars"></i>
-                </span>
-
-            </div>
-            <div className={`nav  h-0 overflow-hidden py-0 duration-300  ${open && '-translate-y-0 h-48'} md:p-3  md:py-3  md:h-10 flex flex-col fixed top-10 left-0 bg-black gap-3 w-screen md:w-auto  md:static  md:flex-row `} >
+            <div className={`nav  h-0 overflow-hidden py-0 px-3 duration-300  ${open && ' h-48'} md:p-3   md:py-3  md:h-10 flex flex-col justify-end md:items-center  top-28 left-0  gap-3 w-screen   md:static  md:flex-row `} >
                 <a href="#"> - Home</a>
                 <a href="#"> - About</a>
                 <a href="#"> - Sermons</a>
                 <a href="#"> - Gallery</a>
                 <a href="#"> - Contact</a>
             </div>
+
+            <div>
+                <span className={`menu-btn md:hidden text-gray-400 self-center translate-y-6 duration-500 text-3xl ${!open && " -scale-y-100"} h-12 w-12 bg-black flex items-center justify-center rounded-full`} onClick={(): void => { setOpen(prev => !prev); console.log(open) }}>
+                    ^
+                </span>
+            </div>
         </div >
     )
 }
 
-// const Navbar = () => {
-//     const [menu, setMenu] = useState<boolean>(false)
 
-//     return (
-//         <div className='flex flex-row gap-2 justify-between w-screen items-center  p-3  md:px-10 bg-slate-900 text-gray-400'>
-//             <p>FFCM</p>
-//             <button className='"menu-btn md:hidden' onClick={() => { setMenu(prevState => !prevState); console.log(menu) }}>MENU</button>
-//             <div className={`menu -translate-y-72 ${menu && '-translate-y-0'}`}>
-//                 <a href="#">Home</a>
-//                 <a href="#">About</a>
-//                 <a href="#">Sermons</a>
-//                 <a href="#">Gallery</a>
-//                 <a href="#">Contact</a>
-//             </div>
-//         </div>
-
-//     )
-// }
 
 export default Navbar
