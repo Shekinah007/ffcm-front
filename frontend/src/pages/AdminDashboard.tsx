dfaa3bcd9859b4022de9a7005f969e76a97e038a
Shekinah Shalom-Hope <shalomsheks@gmail.com>
2023-10-28 06:13:39 +0100

    Save accessToken to localStorage - Retrieve accessToken from localStoge in dashboard to get user profile

--- frontend/src/pages/AdminDashboard.tsx
+++ frontend/src/pages/AdminDashboard.tsx
@@ -1,11 +1,53 @@
+import { useEffect, useState } from "react"
 import Navbar from "../components/Navbar"
+import { toast } from "react-toastify"
 
 const AdminDashboard = () => {
+
+    const [username, setUsername] = useState<string>("")
+    const [firstName, setFirstName] = useState<string>("")
+    const [lastName, setLastName] = useState<string>("")
+    const accessToken = localStorage.getItem('accessToken')
+
+
+    useEffect(() => {
+        console.log("Access token", accessToken)
+
+        fetch("http://localhost:3000/user/profile", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${accessToken}`
+            }
+        }).then(res => {
+            if (res.ok) {
+                toast("Success")
+                return res.json()
+            } else {
+                toast("Error")
+                return ""
+            }
+        }).then(data => {
+            if (data) {
+                console.log("Data ", data)
+                setUsername(data.username)
+                setFirstName(data.firstName)
+                setLastName(data.lastName)
+
+
+            }
+        })
+    }, [])
+
+
     return (
-        <div className="admin-dashboard">
+        <div className="admin-dashboard flex flex-col ">
             <Navbar />
-            <div className="pt-14">
-                <p>Welcome Back</p>
+            <div className="pt-20 flex flex-col bg-gray-200 p-2">
+                <p className="text-xl font-bold absolute top-12 md:top-0 md:relative  ">Welcome</p>
+                <div className="text-black text-2xl text-center">
+                    <p>{firstName && firstName} {lastName && lastName}</p>
+                </div>
             </div>
         </div>
     )
--- frontend/src/pages/SignIn.tsx
+++ frontend/src/pages/SignIn.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import { Link, useNavigate } from "react-router-dom"
 import { toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css'
 
 
 
@@ -8,22 +9,35 @@ const SignIn = () => {
 
     let navigate = useNavigate()
 
-    const [username, setUsername] = useState("");
+    const [username, setUsername] = useState<string>("");
     const [password, setPassword] = useState<string>("");
+    const [validationError, setValidationError] = useState<Boolean>(false)
 
 
 
 
-    function signInAdmin(e: any) {
+    async function signInAdmin(e: any) {
+
         e.preventDefault();
 
         const emailReg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+        console.log("hehwke", username.match(emailReg))
 
 
-
-        if (username == "" || password == "") {
+        if (username.match(emailReg) == null) {
+            toast('Enter a valid email address. E.g "justice@example.com"')
+            setValidationError(true)
             return;
         }
+
+        if (username == "" || password == "") {
+            console.log("Hello")
+            toast("None of the fields should be empty")
+            setValidationError(true)
+            return;
+        }
+
+
 
 
         if (username.match(emailReg)) {
@@ -34,21 +48,24 @@ const SignIn = () => {
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify(loginData)
             }).then(res => {
+                if (!res) {
+                    toast("Go and buy data")
+                }
                 if (res.ok) {
                     console.log("Login successful!!")
                     toast("Login Successfull")
                     return res.json()
                 } else {
                     toast("Login failed!!")
-                    console.log(res)
+                    // console.log(res)
                     return ""
                 }
             }).then(data => {
                 if (!data) {
                     return ""
                 }
-                console.log("Login Success!", data)
-                console.log(data.accessToken)
+                // console.log("Login Success!", data)
+                // console.log(data.accessToken)
                 localStorage.setItem("username", data.username)
                 localStorage.setItem("accessToken", data.accessToken)
                 setTimeout(() => {
@@ -65,7 +82,7 @@ const SignIn = () => {
             <div>
 
             </div>
-            <div className="rounded-lg bg-yellow-400/90 text-white h-[400px] w-[300px] flex flex-col items-center justify-center gap-3">
+            <div className={`rounded-lg bg-yellow-400/90 transition-all duration-1000 ${validationError && "bg-gray-700/80 border-8 border-gray-800 "}  text-white h-[400px] w-[300px] flex flex-col items-center justify-center gap-3`}>
                 <div className="">
                     <h2 className="font-semibold text-xl text-center">Admin Login</h2>
                     <hr className="w-[200px] mt-1 mb-5" />
@@ -75,7 +92,10 @@ const SignIn = () => {
                         <label htmlFor="username"></label>
                         <input
                             value={username}
-                            onChange={(e) => setUsername(e.target.value)}
+                            onChange={(e) => {
+                                setValidationError(false)
+                                setUsername(e.target.value)
+                            }}
                             id="username" type="string" required placeholder="Email" className="p-2 text-black rounded-md" />
                     </div>
 
@@ -83,7 +103,10 @@ const SignIn = () => {
                         <label htmlFor="password"></label>
                         <input
                             value={password}
-                            onChange={(e) => setPassword(e.target.value)}
+                            onChange={(e) => {
+                                setValidationError(false)
+                                setPassword(e.target.value)
+                            }}
                             id="password" type="password" required minLength={4} placeholder="Password" className="p-2 text-black rounded-md" />
                     </div>
                     <hr className="w-[200px] mt-5 " />
